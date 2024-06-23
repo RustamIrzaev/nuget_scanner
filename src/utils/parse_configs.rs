41eec1b376c054a378e77670e50c976d9c49c09d
Rustam Irzaiev <irzaevrustam@gmail.com>
2024-06-23 15:41:34 +0200

    removed duplicate logic

--- src/utils/parse_configs.rs
+++ src/utils/parse_configs.rs
@@ -3,27 +3,21 @@ use std::io::{BufRead, BufReader};
 use regex::Regex;
 
 pub fn parse_packages_config(file_path: &str) -> Vec<(String, String)> {
-    let mut packages = Vec::new();
-    let file = File::open(file_path).expect("Unable to open file");
-    let reader = BufReader::new(file);
     let regex = Regex::new(r#"<package id="([^"]+)" version="([^"]+)""#).unwrap();
-
-    for line in reader.lines() {
-        if let Ok(line) = line {
-            if let Some(captures) = regex.captures(&line) {
-                packages.push((captures[1].to_string(), captures[2].to_string()));
-            }
-        }
-    }
-
-    packages
+    
+    extract_packages_info_from_file(file_path, regex)
 }
 
 pub fn parse_csproj(file_path: &str) -> Vec<(String, String)> {
+    let regex = Regex::new(r#"<PackageReference Include="([^"]+)" Version="([^"]+)""#).unwrap();
+
+    extract_packages_info_from_file(file_path, regex)
+}
+
+fn extract_packages_info_from_file(file_path: &str, regex: Regex) -> Vec<(String, String)> {
     let mut packages = Vec::new();
     let file = File::open(file_path).expect("Unable to open file");
     let reader = BufReader::new(file);
-    let regex = Regex::new(r#"<PackageReference Include="([^"]+)" Version="([^"]+)""#).unwrap();
 
     for line in reader.lines() {
         if let Ok(line) = line {
